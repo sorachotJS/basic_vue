967a73653905acf21af84d3e1fa5e4237f41ac41
Sarachot Jaisat <sarachot.jaisat@namiki.co.th>
2022-12-11 13:32:43 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<style>
+</style>
